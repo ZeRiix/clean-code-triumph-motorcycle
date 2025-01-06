84bef57063dc9e5901c009153561278d9219d095
ZeRiix <willflo46@gmail.com>
2025-01-06 16:15:51 +0100

    refactor: Update TryEntity and Warranty classes for improved readability and consistency

--- domains/entities/warrantyEntity.ts
+++ domains/entities/warrantyEntity.ts
@@ -1,28 +1,28 @@
 /*
- * Justification: Managing warranties and corrective actions in the event 
- *      of failure requires a separate business entity to structure 
+ * Justification: Managing warranties and corrective actions in the event
+ *      of failure requires a separate business entity to structure
  *      this information.
 */
 
 export interface WarrantyDefinition {
-    startDate: Date;
-    endDate: Date;
-    description?: string;
+	startDate: Date;
+	endDate: Date;
+	description?: string;
 }
 
 export class Warranty {
-    private constructor(
-        public readonly definition: WarrantyDefinition,
-    ) { }
+	private constructor(
+		public readonly definition: WarrantyDefinition,
+	) { }
 
-    public static create(definition: WarrantyDefinition): Warranty {
-        return new Warranty(definition);
-    }
+	public static create(definition: WarrantyDefinition): Warranty {
+		return new Warranty(definition);
+	}
 
-    public extendWarranty(endDate: Date): Warranty {
-        return new Warranty({
-            ...this.definition,
-            endDate,
-        });
-    }
+	public extendWarranty(endDate: Date): Warranty {
+		return new Warranty({
+			...this.definition,
+			endDate,
+		});
+	}
 }
