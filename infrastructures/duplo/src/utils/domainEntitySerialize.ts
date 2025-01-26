1f85ef26bca25eb500a61541b37c821bd43e7528
mathcovax <campani.mathieu@gmail.com>
2025-01-26 14:59:49 +0000

    feat: Ajouter une fonction de sérialisation pour les entités de domaine afin de simplifier la gestion des valeurs des objets

---
+++ infrastructures/duplo/src/utils/domainEntitySerialize.ts
@@ -0,0 +1,25 @@
+import { type DomainEntity } from "domains/entities";
+import { ValueObject } from "domains/types";
+
+type FlatDefinition<
+	GenericDefinition extends object,
+> = {
+	[Prop in keyof GenericDefinition]: GenericDefinition[Prop] extends ValueObject<string, unknown>
+		? GenericDefinition[Prop]["value"]
+		: GenericDefinition[Prop]
+};
+
+export function domainEntitySerialize<
+	GenericDomainEntity extends DomainEntity<object>,
+>(domainEntity: GenericDomainEntity): FlatDefinition<GenericDomainEntity> {
+	return <never>Object.fromEntries(
+		Object.entries(domainEntity.definition)
+			.map(([key, propertyValue]) => <const>[
+				key,
+				propertyValue instanceof ValueObject
+					? propertyValue.value
+					: propertyValue,
+			])
+		,
+	);
+}
